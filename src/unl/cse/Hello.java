bcf5cb5325ab6e85b29630ce57204e27a8b09412
Hundter Biede <6586509+hbiede@users.noreply.github.com>
2021-05-02 18:49:42 -0500

    Fix typo

--- src/unl/cse/Hello.java
+++ src/unl/cse/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Chris Bourke");
 		System.out.println("Computer Science");
-		System.out.println("University of Nebraska-Lincpoln");
+		System.out.println("University of Nebraska-Lincoln");
 	}
 
 }
